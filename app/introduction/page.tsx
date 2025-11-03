caaf25b43bce403ad19dcee2deb5bad8e67d8620
Yoga <yogacahayq@gmail.com>
2025-11-03 20:01:35 +0700

    modify: introduction_page

--- app/introduction/page.tsx
+++ app/introduction/page.tsx
@@ -12,20 +12,12 @@ const Introduction = () => {
     const router = useRouter();
 
     return (
-        <div className='bg-white w-full h-screen'>
+        <div className='bg-white flex items-center justify-center md:flex-row flex-col gap-2 w-full h-screen'>
             <div className='md:mt-0 mt-20'>
                 <Character />
             </div>
             <div className="p-6">
-                <div className='p-6 w-full md:h-56 h-[369px] md:-mt-52 -mt-10 rounded-xl bg-gradient-to-br from-black/20 to-black/10 backdrop-blur-lg border border-black/20 shadow-xl'>
-                    <TextType
-                        text={["Indonesia dikenal sebagai negeri yang kaya akan kebudayaan, di mana setiap provinsi memiliki keunikan dan warisan tradisi yang berbeda-beda. Melalui lomba ini, kami mengajak generasi muda untuk mengenal, mencintai, dan melestarikan budaya bangsa melalui kreativitas digital. Dari rumah adat, tarian, musik, hingga kearifan lokal, semua menjadi bagian dari identitas yang patut dijaga dan diperkenalkan ke dunia. Mari bersama menumbuhkan semangat kebanggaan terhadap keberagaman budaya Indonesia, sekaligus memperkuat rasa persatuan dalam perbedaan. Inilah saatnya budaya Indonesia bersinar melalui inovasi dan karya generasi penerus bangsa."]}
-                        typingSpeed={50}
-                        pauseDuration={1500}
-                        showCursor={true}
-                        cursorCharacter="|"
-                        className={`text-gray md:text-lg text-sm `}
-                    />
+                <div className='flex flex-col gap-4 '>
                     <AnimatedContent
                         distance={150}
                         direction="vertical"
@@ -35,9 +27,11 @@ const Introduction = () => {
                         animateOpacity
                         scale={1.1}
                         threshold={0.2}
-                        delay={30}
+                        delay={0.1}
                     >
-                        <Button variant={"link"} onClick={() => router.push('/province')} className='cursor-pointer ml-[1250px]'>Jelajahi Peta <ExternalLink /></Button>
+                        <h2 className='w-96 text-gray-900 font-semibold'>Indonesia dikenal sebagai negeri yang kaya akan kebudayaan, di mana setiap provinsi memiliki keunikan dan warisan tradisi yang berbeda-beda. Melalui lomba ini, kami mengajak generasi muda untuk mengenal, mencintai, dan melestarikan budaya bangsa melalui kreativitas digital. Dari rumah adat, tarian, musik, hingga kearifan lokal, semua menjadi bagian dari identitas yang patut dijaga dan diperkenalkan ke dunia. Mari bersama menumbuhkan semangat kebanggaan terhadap keberagaman budaya Indonesia, sekaligus memperkuat rasa persatuan dalam perbedaan. Inilah saatnya budaya Indonesia bersinar melalui inovasi dan karya generasi penerus bangsa.</h2>
+
+                        <Button variant={"link"} onClick={() => router.push('/province')} className='cursor-pointer'>Jelajahi Peta <ExternalLink /></Button>
                     </AnimatedContent>
 
                 </div>
